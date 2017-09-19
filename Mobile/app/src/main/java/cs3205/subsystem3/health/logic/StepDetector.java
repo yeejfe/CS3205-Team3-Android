aede213323fa54f85833cd5ed250c25fa8d09734
yeejfe <yeejfe@hotmail.com>
2017-09-19 14:23:48 +0800

    Update sensitivity threshold

--- Mobile/app/src/main/java/cs3205/subsystem3/health/logic/StepDetector.java
+++ Mobile/app/src/main/java/cs3205/subsystem3/health/logic/StepDetector.java
@@ -9,7 +9,7 @@ public class StepDetector {
     private static final int VEL_RING_SIZE = 10;
 
     // change this threshold according to your sensitivity preferences
-    private static final float STEP_THRESHOLD = 50f;
+    private static final float STEP_THRESHOLD = 80f;
 
     private static final int STEP_DELAY_NS = 250000000;
 
