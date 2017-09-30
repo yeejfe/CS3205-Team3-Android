cba1fc44ab30e2bad430a7eb61a673134f161b40
yeejfe <yeejfe@hotmail.com>
2017-09-30 14:18:56 +0800

    Create generic connection to server

--- Mobile/app/src/main/java/cs3205/subsystem3/health/data/source/remote/RemoteDataSource.java
+++ Mobile/app/src/main/java/cs3205/subsystem3/health/data/source/remote/RemoteDataSource.java
@@ -1,8 +1,37 @@
 package cs3205.subsystem3.health.data.source.remote;
 
+import java.io.InputStream;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.Response;
+
 /**
- * Created by Yee on 09/17/17.
+ * Created by Yee on 09/30/17.
  */
 
 public class RemoteDataSource {
+    public static final String SERVER3_ADDRESS = "https://cs3205-3.comp.nus.edu.sg/upload";
+    private static final String JSON = "application/json";
+
+    private Client client;
+
+    public RemoteDataSource() {
+        client = ClientBuilder.newClient();
+    }
+
+    public Response buildFileUploadRequest(InputStream stream){
+        Invocation.Builder builder = client.target(SERVER3_ADDRESS)
+                // .queryParam() //type
+                //  .queryParam() //epoch unixtime
+                .request();
+        return  builder.post(Entity.entity(stream, JSON));
+    }
+
+    public void close(){
+        if (client != null)
+            client.close();
+    }
 }
