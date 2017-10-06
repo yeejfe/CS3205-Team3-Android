074548844e9772ba23c06c57404fe2f7d44d14ae
yeejfe <yeejfe@hotmail.com>
2017-10-06 11:58:28 +0800

    Add upload steps

--- Mobile/app/src/main/java/cs3205/subsystem3/health/data/source/remote/RemoteDataSource.java
+++ Mobile/app/src/main/java/cs3205/subsystem3/health/data/source/remote/RemoteDataSource.java
@@ -6,14 +6,30 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import cs3205.subsystem3.health.common.core.Timestamp;
 
 /**
  * Created by Yee on 09/30/17.
  */
 
 public class RemoteDataSource {
-    public static final String SERVER3_ADDRESS = "https://cs3205-3.comp.nus.edu.sg/upload/image";
+    public final static String SERVER3_UPLOAD_URL = "https://cs3205-3.comp.nus.edu.sg/upload/";
+    public final static String SERVER3_SESSION_UPLOAD_URL = "https://cs3205-3.comp.nus.edu.sg/session/";
+
+    public static final String TIMESTAMP = "?timestamp=";
+
+    public static final String IMG_UPLOAD_URL = SERVER3_UPLOAD_URL + "/image";
+    private final static String STEP_UPLOAD_URL = SERVER3_SESSION_UPLOAD_URL + "/step/" + TIMESTAMP;
+
+    public static final String FRONT_SLASH = "/";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+    public static final String X_NFC_TOKEN = "x-nfc-token";
+
+
     private static final String JSON = "application/json";
 
     private Client client;
@@ -22,26 +38,33 @@ public class RemoteDataSource {
         client = ClientBuilder.newClient();
     }
 
-    public Response buildFileUploadRequest(InputStream stream, String token, String hash){
+    public Response buildFileUploadRequest(InputStream stream, String token, String hash) {
 //        URL url = new URL(SERVER3_ADDRESS);
 //        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
 //        conn.setRequestProperty("Authorization", "Bearer "+ token);
 //        conn.setRequestProperty("x-nfc-token", hash);
 //        OutputStream os = conn.getOutputStream();
 //        OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
-     //   osw.write
-        long time = System.currentTimeMillis()/1000;
-        Invocation.Builder builder = client.target(SERVER3_ADDRESS+"/"+time)
+        //   osw.write
+        long time = Timestamp.getEpochTimeStamp();
+        Invocation.Builder builder = client.target(IMG_UPLOAD_URL + FRONT_SLASH + time)
                 // .queryParam() //type
                 //  .queryParam() //epoch unixtime
                 .request();
-        return  builder
-                .header("x-nfc-token", hash)
-                .header("Authorization", "Bearer "+token)
+        return builder
+                .header(X_NFC_TOKEN, hash)
+                .header(AUTHORIZATION, BEARER + token)
                 .post(Entity.entity(stream, JSON));
     }
 
-    public void close(){
+    public Response buildStepUploadRequest(InputStream stepsData, String token, String hash) {
+        Invocation.Builder builder = client.target(STEP_UPLOAD_URL + Timestamp.getEpochTimeStamp()).request();
+        Response response = builder.header(AUTHORIZATION, BEARER + token).header(X_NFC_TOKEN, hash).post(
+                        Entity.entity(stepsData, MediaType.APPLICATION_OCTET_STREAM_TYPE));
+        return response;
+    }
+
+    public void close() {
         if (client != null)
             client.close();
     }
